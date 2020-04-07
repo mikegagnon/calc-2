33cd4f4ea7cfc7b38d326a54ec896787c36facaf
Michael Gagnon <mikegagnon@gmail.com>
2020-04-07 04:53:38 -0700

    .

--- static/js/calc.js
+++ static/js/calc.js
@@ -21,7 +21,7 @@ const DEFAULT_CONFIG = {
     doAutoDropThree: true,
     autoDropForPhaseDropThreeVacancies: 0,
     startWithPrizeCards: {
-        0: ["heart", "heart", "heart", "diamond", "diamond", "diamond", "club", "club", "club", "club"],
+        //0: ["heart", "heart", "heart", "diamond", "diamond", "diamond", "club", "club", "club", "club"],
         //0: ["heart", "heart", "heart"],
     }
 
